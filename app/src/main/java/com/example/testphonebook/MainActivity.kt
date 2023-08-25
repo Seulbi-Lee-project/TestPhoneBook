dc6dfcfebe09c3e75389eaad0a02acd541f4e43b
Seulbi-Lee-project <ifssfws@gmail.com>
2023-08-25 15:58:52 +0900

    test : test용 데이터 준비

--- app/src/main/java/com/example/testphonebook/MainActivity.kt
+++ app/src/main/java/com/example/testphonebook/MainActivity.kt
@@ -16,6 +16,17 @@ class MainActivity : AppCompatActivity() {
         var view = binding.root
         setContentView(view)
 
+        // 데이터 원본 준비
+        val dataList = mutableListOf<PhoneBook>()
+        dataList.add(PhoneBook(R.drawable.sample1, "Bella", "010-1234-5678"))
+        dataList.add(PhoneBook(R.drawable.sample1, "Charlie", "010-1234-5678"))
+        dataList.add(PhoneBook(R.drawable.sample2, "Daisy", "010-1234-5678"))
+        dataList.add(PhoneBook(R.drawable.sample3, "Duke", "010-1234-5678"))
+        dataList.add(PhoneBook(R.drawable.sample4, "Max", "010-1234-5678"))
+        dataList.add(PhoneBook(R.drawable.sample5, "Happy", "010-1234-5678"))
+        dataList.add(PhoneBook(R.drawable.sample6, "Luna", "010-1234-5678"))
+        dataList.add(PhoneBook(R.drawable.sample7, "Bob", "010-1234-5678"))
+
 
 
     }
